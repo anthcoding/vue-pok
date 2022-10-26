85d8e0c6b82f7f1671bd3e573c4f6c923b03bb79
Anth Lorenzo <anth@Anths-MacBook-Pro.local>
2022-10-26 12:54:43 -0400

    Pokeapp Completed

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="p-14">
+		<router-link class="flex justify-center text-4xl text-yellow-700" to="/"
+			>Pokemon Picker</router-link
+		>
+	</div>
+	<router-view />
+</template>
+
+<style></style>
